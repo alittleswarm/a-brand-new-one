10c42e0c355631ce6e407e89dff488ca12ba145c
abeeswarm <3524201453@qq.com>
2023-08-05 16:00:42 +0800

    a commit

--- 基础阶段/有基础者/5.py
+++ 基础阶段/有基础者/5.py
@@ -1,1 +1,10 @@
 # 写一个正则表达式，用于验证用户密码，长度在6~18 之间，只能包含英文和数字
+import re
+while True:
+    passwd =input('请输入密码:')
+    flag =re.match(r"^[0-9a-zA-Z]{6,18}$",passwd)
+    if flag:
+        break
+    print("格式错误，请重新输入")
+
+print("OK")
